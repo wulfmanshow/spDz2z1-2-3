2aa81b0b72fefab06084ff7b1a96d965d14d137a
Nikita-Poshtaruk <fanadeich@gmail.com>
2023-04-07 14:10:38 +0300

    Добавьте файлы проекта.

---
+++ spDz2z1-2-3/Program.cs
@@ -0,0 +1,31 @@
+﻿//задание1
+//using System.Diagnostics;
+
+//internal class Program
+//{
+//    private static void Main(string[] args)
+//    {
+//        Process process = new Process();
+//        process.StartInfo.FileName = "mspaint.exe";
+//        process.Start();
+//        Console.ReadLine();
+//        process.Kill();
+//    }
+//}
+//-------------------------------------------------------------------------------
+//задание 2
+//using System.Diagnostics;
+
+//internal class Program
+//{
+//    private static void Main(string[] args)
+//    {
+//        Process process = new Process();
+//        process.StartInfo.FileName = "mspaint.exe";
+//        process.Start();
+//        Console.ReadLine();
+//        process.Kill();
+//    }
+//}
+//-------------------------------------------------------------------------
+//задание 3 не понял как сделать
